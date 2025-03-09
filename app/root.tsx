25454457091fb87452ec9d5c383f656fe339a9cd
Emilio Ramírez Mascarúa <ramarm0825@gmail.com>
2025-03-09 13:07:37 -0600

    Meta tag for telegram (#17)
    * Fix(landing-page): Remove the template front
    
    * Feature(landing-page): Shadcn/ui implementation
    
    * Feature(landing-page): Tailwind coloras
    
    * Feature(tailwind):colors
    
    * feature(landing-navbar): Finished styling navbar
    
    * Feature(landing-page): FInal comit for landing page
    
    icon
    
    * Feature(landing-page): Landing page finished for mobil and desktop (#12)
    
    * Feature(meta-tages): Links have meta tags now (#14)
    
    * Meta tags (#16)
    
    * Feature(meta-tages): Links have meta tags now
    
    * meta-tags

--- app/root.tsx
+++ app/root.tsx
@@ -39,7 +39,11 @@ export function meta() {
       content: "Sweet adventures start with planning",
     },
     { property: "og:type", content: "website" },
-
+    {
+      property: "og:image",
+      content: "https://honeytrip.emascarua.uk/alexMegosEating.jpg",
+    },
+    { property: "og:url", content: "https://honeytrip.emascarua.uk" },
     // Twitter Card tags
     { name: "twitter:card", content: "summary" },
     { name: "twitter:title", content: "Honey Trip" },
