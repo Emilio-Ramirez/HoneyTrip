cead2eefaac27d8a3659e4ed95a079ad8f07b3ab
Emilio Ramírez Mascarúa <ramarm0825@gmail.com>
2025-03-09 12:24:46 -0600

    Feature(landing-page): Landing page finished for mobil and desktop (#12)

--- app/root.tsx
+++ app/root.tsx
@@ -18,6 +18,7 @@ export const links: Route.LinksFunction = () => [
     href: "https://fonts.gstatic.com",
     crossOrigin: "anonymous",
   },
+  { rel: "icon", href: "/favicon1.ico", type: "image/x-icon" },
   {
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
